19898cf3d66ce174bd037ad0174752cb77c0e0b8
Robertudo <sayamosfero@gmail.com>
2020-10-25 16:37:36 -0600

    Cambio de fragments a Activity

---
+++ app/src/main/java/com/example/express/ContactosActivity.java
@@ -0,0 +1,71 @@
+package com.example.express;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ContactosActivity extends AppCompatActivity {
+
+    BottomNavigationView mBottomNavigationView;
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contactos);
+
+        mBottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        mBottomNavigationView.setSelectedItemId((R.id.menu_contacts));
+
+        mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                if(item.getItemId() == R.id.menu_home){
+                    Intent i = new Intent(getApplicationContext(), InicioActivity.class);
+                    startActivity(i);
+                    overridePendingTransition(0,0);
+                }
+
+                if(item.getItemId() == R.id.menu_account){
+                    Intent i = new Intent(getApplicationContext(), MiPerfilActivity.class);
+                    startActivity(i);
+                    overridePendingTransition(0,0);
+                }
+
+                return true;
+            }
+        });
+    }
+
+    // MOSTRAR TOP MENU
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.top_menu,menu);
+        return true;
+    }
+
+    // SWITCH DEL TOP MENU
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.btnSignOut:
+                //Detectar al usuario actual
+                mAuth = FirebaseAuth.getInstance();
+
+                //SIGN OUT
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/example/express/InicioActivity.java
+++ app/src/main/java/com/example/express/InicioActivity.java
@@ -13,9 +13,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.example.express.fragments.AccountFragment;
-import com.example.express.fragments.ContactsFragment;
-import com.example.express.fragments.HomeFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -29,24 +26,24 @@ public class InicioActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
 
-        showSelectedFragment(new HomeFragment());
-
         mBottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        mBottomNavigationView.setSelectedItemId((R.id.menu_home));
 
         mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-                if(item.getItemId() == R.id.menu_home){
-                    showSelectedFragment(new HomeFragment());
-                }
-
                 if(item.getItemId() == R.id.menu_contacts){
-                    showSelectedFragment(new ContactsFragment());
+                    Intent i = new Intent(getApplicationContext(), ContactosActivity.class);
+                    startActivity(i);
+                    overridePendingTransition(0,0);
                 }
 
                 if(item.getItemId() == R.id.menu_account){
-                    showSelectedFragment(new AccountFragment());
+                    Intent i = new Intent(getApplicationContext(), MiPerfilActivity.class);
+                    startActivity(i);
+                    overridePendingTransition(0,0);
                 }
 
                 return true;
@@ -54,18 +51,15 @@ public class InicioActivity extends AppCompatActivity {
         });
     }// Fin on Create
 
-    private void showSelectedFragment(Fragment fragment){
-        getSupportFragmentManager().beginTransaction().replace(R.id.container_frame, fragment)
-                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
-                .commit();
-    }
 
+    // MOSTRAR TOP MENU
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.top_menu,menu);
         return true;
     }
 
+    // SWITCH DEL TOP MENU
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()){
@@ -75,7 +69,7 @@ public class InicioActivity extends AppCompatActivity {
 
                 //SIGN OUT
                 FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(InicioActivity.this,MainActivity.class));
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
                 break;
         }
         return super.onOptionsItemSelected(item);
