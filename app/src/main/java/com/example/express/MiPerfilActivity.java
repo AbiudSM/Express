a3e446592288325e9c724adac4a37e8bd0e5a119
Robertudo <sayamosfero@gmail.com>
2020-10-26 02:08:46 -0600

    Mandar llamar datos del real time database de Firebase

--- app/src/main/java/com/example/express/MiPerfilActivity.java
+++ app/src/main/java/com/example/express/MiPerfilActivity.java
@@ -7,14 +7,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 
+import com.example.express.models.Usuario;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MiPerfilActivity extends AppCompatActivity {
 
     BottomNavigationView mBottomNavigationView;
     FirebaseAuth mAuth;
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    EditText userNombre, userTelefono, userProfesion, userServicios, userCotizacion, userDescripcion;
+    public String uid = user.getUid();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +37,17 @@ public class MiPerfilActivity extends AppCompatActivity {
         setContentView(R.layout.activity_mi_perfil);
 
         mBottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
-
         mBottomNavigationView.setSelectedItemId((R.id.menu_account));
+
+        userNombre = findViewById(R.id.txt_nombre_perfil);
+        userTelefono = findViewById(R.id.txt_telefono_perfil);
+        userProfesion = findViewById(R.id.txt_profesion_perfil);
+        userServicios = findViewById(R.id.txt_servicios_perfil);
+        userCotizacion = findViewById(R.id.txt_cotizacion_perfil);
+        userDescripcion = findViewById(R.id.txt_descripcion_perfil);
+
+        inicializarFirebase();
+        MostrarDatos();
 
         mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -44,6 +68,33 @@ public class MiPerfilActivity extends AppCompatActivity {
                 return true;
             }
         });
+    }
+
+    private void inicializarFirebase() {
+        FirebaseApp.initializeApp(this);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+    }
+
+    private void MostrarDatos() {
+        databaseReference.child("Usuario").child(uid).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Usuario u = snapshot.getValue(Usuario.class);
+                userNombre.setText(u.getNombre());
+                userTelefono.setText(u.getTelefono());
+                userProfesion.setText(u.getProfesion());
+                userServicios.setText(u.getServicios());
+                userCotizacion.setText(u.getCotizacion());
+                userDescripcion.setText(u.getDescripcion());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 
     // MOSTRAR TOP MENU
